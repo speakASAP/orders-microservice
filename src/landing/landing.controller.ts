f80ddda4a803840c17c5cd2c8ce6f4e3293ce7ef
speakASAP <ssfskype@gmail.com>
2026-07-17 22:44:19 +0200

    favicon update

--- src/landing/landing.controller.ts
+++ src/landing/landing.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get, Header, StreamableFile } from '@nestjs/common';
 import { Public } from '../auth/roles.decorator';
 import { LANDING_HTML } from './landing-ui';
+import { FAVICON_ICO } from './favicon.assets';
 
 @Controller()
 export class LandingController {
@@ -9,5 +10,13 @@ export class LandingController {
   @Header('Content-Type', 'text/html; charset=utf-8')
   getLanding() {
     return LANDING_HTML;
+  }
+
+  @Public()
+  @Get('favicon.ico')
+  @Header('Content-Type', 'image/x-icon')
+  @Header('Cache-Control', 'public, max-age=604800')
+  getFavicon() {
+    return new StreamableFile(FAVICON_ICO);
   }
 }
